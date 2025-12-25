eccb8a065418d71ce80616dae9e51113995c32cd
Wojciech Dranka <75485345+wdranka@users.noreply.github.com>
2025-12-25 14:05:08 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'rotas-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
